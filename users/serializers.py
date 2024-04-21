57b982b0e30c52c7bda1c70535df36cd24f54409
Egor Fironov <131647674+Egor1511@users.noreply.github.com>
2024-04-21 23:34:06 +0700

    add serializers for auth code

--- users/serializers.py
+++ users/serializers.py
@@ -5,3 +5,10 @@ class PhoneNumberInputSerializer(serializers.Serializer):
     phone_number = serializers.CharField(
         max_length=15,
     )
+
+
+class AuthCodeInputSerializer(serializers.Serializer):
+    auth_code = serializers.CharField(
+        max_length=4,
+        min_length=4,
+    )
